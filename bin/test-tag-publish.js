523d79cdeffc1c2f78bfc627f5a44bcf61c6dcdb
Tony Brix <tony@brix.ninja>
2017-03-15 09:26:37 -0500

    Initial commit

---
+++ bin/test-tag-publish.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+
+const spawn = require("child_process").spawn;
+const path = require("path");
+const argv = require("minimist")(process.argv.slice(2));
+const Colors = require("colors/safe");
+
+function error(str) {
+	console.log(Colors.red("Error: " + Array.from(arguments).join(" ")));
+	process.exit(1);
+};
+
+function success() {
+	console.log(Colors.green(Array.from(arguments).join(" ")));
+};
+
+function output() {
+	console.log(Colors.blue(Array.from(arguments).join(" ")));
+};
+
+function done() {
+	process.exit(0);
+};
+
+function getCurrentVersion() {
+	try {
+		const pJson = require.resolve(path.resolve(process.cwd(), "./package.json"));
+		delete require.cache[pJson];
+		const pkg = require(pJson);
+		return pkg.version;
+	} catch (e) {
+		error("Could not open a package.json.");
+	}
+};
+
+function getMessage(message) {
+	return "\"" + message.replace(/%s/g, newVersion) + "\"";
+}
+
+function run(cmd, args, callback) {
+	output(">", cmd, ...args);
+	const proc = spawn(cmd, args, { shell: true });
+	proc.stdout.pipe(process.stdout);
+	proc.stderr.pipe(process.stderr);
+	proc.on("close", callback);
+}
+
+function npmTest() {
+	output("Testing...");
+	run("npm", ["test"], function (code) {
+		if (code !== 0) return error("Testing failed.");
+		success("Testing completed.");
+		npmVersion();
+	});
+}
+
+function npmVersion() {
+	output("Updating version...");
+	run("npm", ["--no-git-tag-version", "version", version], function (code) {
+		if (code !== 0) return error("Updating version failed.");
+		newVersion = getCurrentVersion();
+		success("Version updated from", oldVersion, "to", newVersion);
+		gitCommit();
+	});
+}
+
+function gitCommit() {
+	output("Commiting...");
+	run("git", ["commit", "--all", "-m", getMessage(message)], function (code) {
+		if (code !== 0) return error("Commiting failed.");
+		success("Commiting completed.");
+		gitTag();
+	});
+}
+
+function gitTag() {
+	output("Tagging...");
+	run("git", ["tag", "-a", "v" + newVersion, "-m", getMessage(tagMessage)], function (code) {
+		if (code !== 0) return error("Tagging failed.");
+		success("Tagging completed.");
+		gitPush();
+	});
+}
+
+function gitPush() {
+	output("Pushing...");
+	run("git", ["push", "--follow-tags"], function (code) {
+		if (code !== 0) return error("Pushing failed.");
+		success("Pushing completed.");
+		done(); // TODO:
+		npmPublish();
+	});
+}
+
+function npmPublish() {
+	output("Publishing...");
+	run("npm", ["publish"], function (code) {
+		if (code !== 0) return error("npm publish failed.");
+		success("Released new version", newVersion, "successfully.");
+		done();
+	});
+}
+
+const oldVersion = getCurrentVersion();
+let newVersion;
+const version = process.argv[2];
+// %s is npm version variable: https://docs.npmjs.com/cli/version
+const message = argv.m || argv.message || "v%s";
+const tagMessage = argv.t || argv.tag || message;
+
+if (!oldVersion) {
+	error("No version in package.json found.");
+}
+if (!version) {
+	error("No version supplied.");
+}
+
+npmTest();
