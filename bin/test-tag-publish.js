520ec51d3ae30a7b6232c3c9784f0aa97d90331b
Tony Brix <Tony@Brix.ninja>
2017-03-17 12:36:10 -0500

    allow skipping tests

--- bin/test-tag-publish.js
+++ bin/test-tag-publish.js
@@ -2,7 +2,7 @@
 
 const spawn = require("child_process").spawn;
 const path = require("path");
-const argv = require("minimist")(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(3));
 const Colors = require("colors/safe");
 
 function error(str) {
@@ -22,16 +22,16 @@ function done() {
 	process.exit(0);
 };
 
-function getCurrentVersion() {
+function getPkg() {
 	try {
 		const pJson = require.resolve(path.resolve(process.cwd(), "./package.json"));
 		delete require.cache[pJson];
 		const pkg = require(pJson);
-		return pkg.version;
+		return pkg;
 	} catch (e) {
 		error("Could not open a package.json.");
 	}
-};
+}
 
 function getMessage(message) {
 	return "\"" + message.replace(/%s/g, newVersion) + "\"";
@@ -46,21 +46,28 @@ function run(cmd, args, callback) {
 }
 
 function start() {
-	if (force) {
-		return npmTest();
-	}
+	if (force) return npmTest();
+
 	output("Checking for uncommitted changes...");
 	run("git", ["diff", "--quiet"], function (code) {
 		if (code !== 0) return error("There are uncommitted changes in the working tree. To skip this check use -f.");
+
 		success("Checking completed.");
 		npmTest();
 	});
 }
 
 function npmTest() {
+	if (noTest) return npmVersion();
+
 	output("Testing...");
+	const pkg = getPkg();
+	if (!pkg.scripts || !pkg.scripts.test) {
+		error("No tests found. To skip this check use -n.");
+	}
 	run("npm", ["test"], function (code) {
 		if (code !== 0) return error("Testing failed.");
+
 		success("Testing completed.");
 		npmVersion();
 	});
@@ -70,7 +77,8 @@ function npmVersion() {
 	output("Updating version...");
 	run("npm", ["--no-git-tag-version", "version", version], function (code) {
 		if (code !== 0) return error("Updating version failed.");
-		newVersion = getCurrentVersion();
+
+		newVersion = getPkg().version;
 		success("Version updated from", oldVersion, "to", newVersion);
 		gitCommit();
 	});
@@ -80,6 +88,7 @@ function gitCommit() {
 	output("Committing...");
 	run("git", ["commit", "--all", "-m", getMessage(message)], function (code) {
 		if (code !== 0) return error("Committing failed.");
+
 		success("Committing completed.");
 		gitTag();
 	});
@@ -89,6 +98,7 @@ function gitTag() {
 	output("Tagging...");
 	run("git", ["tag", "-a", "v" + newVersion, "-m", getMessage(tagMessage)], function (code) {
 		if (code !== 0) return error("Tagging failed.");
+
 		success("Tagging completed.");
 		gitPush();
 	});
@@ -98,6 +108,7 @@ function gitPush() {
 	output("Pushing...");
 	run("git", ["push", "--follow-tags"], function (code) {
 		if (code !== 0) return error("Pushing failed.");
+
 		success("Pushing completed.");
 		npmPublish();
 	});
@@ -107,18 +118,19 @@ function npmPublish() {
 	output("Publishing...");
 	run("npm", ["publish"], function (code) {
 		if (code !== 0) return error("npm publish failed.");
+
 		success("Released new version", newVersion, "successfully.");
 		done();
 	});
 }
 
-const oldVersion = getCurrentVersion();
-let newVersion;
 const version = process.argv[2];
-// %s is npm version variable: https://docs.npmjs.com/cli/version
+const oldVersion = getPkg().version;
+let newVersion;
 const message = argv.m || argv.message || "v%s";
 const tagMessage = argv.t || argv.tag || message;
 const force = argv.f || argv.force;
+const noTest = argv.n || (argv.test === false);
 
 if (!oldVersion) {
 	error("No version in package.json found.");
